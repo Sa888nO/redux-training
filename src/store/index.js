4ca189f4a83001b180a3377dd63e1ae409b7de28
Sa888nO <shura.reno.97@bk.ru>
2022-06-22 00:32:28 +0300

    lesson 3
    Научился взаимодействовать одновременно с несколькими редьюсерами одновременно с помощью combineReducers. Так же научился использовать redux devtools, для просмотра изменений state

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "redux";
+import { cashReducer } from "./cashReducer";
+import { customerReducer } from "./customerReducer";
+import { combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const rootReducer = combineReducers({
+  cash: cashReducer,
+  customers: customerReducer,
+});
+export const store = createStore(rootReducer, composeWithDevTools());
